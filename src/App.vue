dc116db01836e55167aeddab38bacbb967162449
adceballos <78570037+adceballos@users.noreply.github.com>
2025-05-29 16:05:24 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import DeckCard from './components/DeckCard.vue'
+</script>
+
+<template>
+  <DeckCard :deckNum="1" title="Basics"/>
+  <HelloWorld msg="Vite + Vue" />
+</template>
---
+++ src/components/DeckCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+defineProps<{ 
+  deckNum: number
+  title: string
+  // routePath: string
+}>()
+
+</script>
+
+<template>
+  <div class="flex flex-col px-4 pb-12 pt-4 items-start bg-gray-500 rounded-2xl shadow-xl m-6 gap-y-2">
+    <h1 class="text-5xl">Deck {{ deckNum }}</h1>
+    <h2 class="text-3xl text-blue-300">{{ title }}</h2>
+    <p class="text-xl">Greetings and Politeness</p>
+  </div>
+</template>
