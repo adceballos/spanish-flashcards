d3c17c8a63e82a70f72293e9141bf1d267829a08
adceballos <78570037+adceballos@users.noreply.github.com.]>
2025-07-08 14:39:18 -0500

    feat: add initial spanish flashcard decks to JSON database

--- frontend/src/components/DeckCard.vue
+++ frontend/src/components/DeckCard.vue
@@ -16,7 +16,7 @@ defineProps<{
     <div class="flex flex-col pl-4 pr-20 pb-10 pt-4 items-start bg-gray-500 rounded-2xl shadow-xl gap-y-2 transition transformation hover:scale-105 hover:shadow-2xl duration-300 hover:cursor-pointer">
       <h1 class="text-5xl">Deck {{ deckNum }}</h1>
       <h2 class="text-3xl text-blue-300">{{ title }}</h2>
-      <p class="text-xl">{{ description }}</p>
+      <p class="text-md">{{ description }}</p>
     </div>
   </RouterLink>
 </template>
