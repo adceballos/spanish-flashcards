affdad8588080572ef5a2831106ffce2f51308ee
adceballos <78570037+adceballos@users.noreply.github.com.]>
2025-07-11 22:22:52 -0500

    feat: create basic header component

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <header class="bg-gray-500 py-5">
+      <nav class="flex items-center justify-center gap-x-4">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  </header>
+</template>
